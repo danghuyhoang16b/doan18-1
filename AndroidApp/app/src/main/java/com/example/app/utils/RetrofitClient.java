20c6ce1d057d6872439300e203f5eb1edae73d02
danghuyhoang16b <hoangnd30082003@gmail.com>
2026-01-14 15:35:21 +0700

    fix(retrofit): bật Gson lenient để tránh lỗi JsonReader khi server trả JSON không chuẩn

--- AndroidApp/app/src/main/java/com/example/app/utils/RetrofitClient.java
+++ AndroidApp/app/src/main/java/com/example/app/utils/RetrofitClient.java
@@ -2,6 +2,8 @@ package com.example.app.utils;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class RetrofitClient {
     private static Retrofit retrofit = null;
@@ -11,18 +13,20 @@ public class RetrofitClient {
 
     public static Retrofit getClient() {
         if (retrofit == null) {
+            Gson gson = new GsonBuilder().setLenient().create();
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
         }
         return retrofit;
     }
 
     public static void overrideForTesting(String baseUrl) {
+        Gson gson = new GsonBuilder().setLenient().create();
         retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
     }
 }
